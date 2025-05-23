8d9e8e4f99953cc2b473699df11cc0a46a947905
majafranz <majafranz99@gmail.com>
2025-05-23 11:04:08 +0200

    move seeker location on click

--- components/map.vue
+++ components/map.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import L from 'leaflet'
-import { LMap, LTileLayer, LMarker, LIcon } from '@vue-leaflet/vue-leaflet'
+import { LMap, LTileLayer, LMarker, LPopup } from '@vue-leaflet/vue-leaflet'
 
 const props = defineProps<{
 	hider_location: { lat: number; lng: number }
@@ -9,40 +9,49 @@ const props = defineProps<{
 }>()
 
 const map = ref(null)
-const markers = ref<L.Marker[]>([])
-const center = ref<[number, number]>([49.00252018672146, 12.100188665384959])
+
+const center = ref<[number, number]>([props.hider_location.lat, props.hider_location.lng])
 const zoom = 16
 
-const move_center = (lat, lng) => {
-	const leafletMap = map.value.leafletObject
-	leafletMap.flyTo([lat, lng], zoom)
-}
+const seekerPosition = ref<[number, number]>([props.seeker_location.lat, props.seeker_location.lng])
+const hiderPosition = ref<[number, number]>([props.hider_location.lat, props.hider_location.lng])
 
-const onMapReady = () => {
+const move_center = (lat: number, lng: number) => {
+	map.value?.leafletObject.flyTo([lat, lng], zoom)
 }
 
 const onMapClick = (event: any) => {
+	// Move seeker marker
 	const { lat, lng } = event.latlng
-
-	const name = prompt('Enter name for this location:')
-	if (!name) return
-
-	const marker = L.marker([lat, lng]).bindPopup(name).addTo(map.value.leafletObject)
-	markers.value.push(marker)
+	seekerPosition.value = [lat, lng]
 	move_center(lat, lng)
 }
+
+// // On click for marker creation
+// const onMapClick = (event: any) => {
+// 	const { lat, lng } = event.latlng
+// 
+// 	const name = prompt('Enter name for this location:')
+// 	if (!name) return
+// 
+// 	const marker = L.marker([lat, lng]).bindPopup(name).addTo(map.value.leafletObject)
+// 	markers.value.push(marker)
+// 	move_center(lat, lng)
+// }
 </script>
 
 <template>
-	<LMap ref="map" style="height: 100vh; width: 100vw" :zoom="16" :center="center" :use-global-leaflet="true"
-		@ready="onMapReady" @click="onMapClick">
+	<LMap ref="map" style="height: 100vh; width: 100vw" :zoom="zoom" :center="center" :use-global-leaflet="true"
+		@click="onMapClick">
 		<LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			attribution="&copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors" />
-		<LMarker :lat-lng="[props.hider_location.lat, props.hider_location.lng]">
-			<LPopup>Hider</LPopup>
-		</LMarker>
-		<LMarker :lat-lng="[props.seeker_location.lat, props.seeker_location.lng]">
+
+		<LMarker :lat-lng="seekerPosition">
 			<LPopup>Seeker</LPopup>
+		</LMarker>
+
+		<LMarker :lat-lng="hiderPosition">
+			<LPopup>Hider</LPopup>
 		</LMarker>
 	</LMap>
 </template>
