010bba48de8115c733370fa6951b8a2fae791565
scm30415 <mathias.schoettle@st.oth-regensburg.de>
2025-05-23 12:13:26 +0200

    hallo

--- components/lobby/wrapper.vue
+++ components/lobby/wrapper.vue
@@ -4,8 +4,17 @@ const api = useApi()
 
 const stateStore = useStateStore()
 
+
+
 </script>
 
 <template>
-	<LobbyRoom :user-id="stateStore.userId" :room-id="stateStore.roomId ?? 'OH OH'" :users="stateStore.users"/>
+	<LobbyRoom
+		:user-id="stateStore.userId"
+		:room-id="stateStore.roomId ?? ''"
+		:users="stateStore.users"
+		@close="api.closeRoom()"
+		@leave="api.leaveRoom()"
+		@make-hider=""
+	/>
 </template>
