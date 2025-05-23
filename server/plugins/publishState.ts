a16fc32c6360a421d90c658fdb5998db2e9d07e8
scm30415 <mathias.schoettle@st.oth-regensburg.de>
2025-05-23 14:04:18 +0200

    removed logging in server

--- server/plugins/publishState.ts
+++ server/plugins/publishState.ts
@@ -2,7 +2,7 @@ import { state } from "../utils/state";
 
 export default defineNitroPlugin(async (_) => {
     setInterval(() => {
-        console.log(JSON.stringify(state, (key, value) => key === "peer" ? "peer" : value, 2));
+        // console.log(JSON.stringify(state, (key, value) => key === "peer" ? "peer" : value, 2));
         state.updateTimers();
         state.publishGameStates();
     }, 1000);
