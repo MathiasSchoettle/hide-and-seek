bc42206c9a4f27d5937b852fe39389ea0f66b048
sith98 <sith9809@gmail.com>
2025-05-23 14:35:57 +0200

    Debug server logs only in dev mode

--- server/plugins/publishState.ts
+++ server/plugins/publishState.ts
@@ -1,8 +1,15 @@
 import { state } from "../utils/state";
 
 export default defineNitroPlugin(async (_) => {
+    let prevState: string | undefined = undefined;
     setInterval(() => {
-        // console.log(JSON.stringify(state, (key, value) => key === "peer" ? "peer" : value, 2));
+        if (import.meta.dev) {
+            const newUserState = JSON.stringify(state, (key, value) => key === "peer" ? "peer" : value, 2);
+            if (newUserState !== prevState) {
+                console.log(newUserState);
+                prevState = newUserState;
+            }
+        }
         state.updateTimers();
         state.publishGameStates();
     }, 1000);
